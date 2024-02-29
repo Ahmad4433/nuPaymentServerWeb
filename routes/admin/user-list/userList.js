2da07b928adc591666e6543e3cf0e230b4a52bba
Ahmad4433 <aliraza126005#gmail.com>
2024-02-29 19:08:51 +0500

    sdssd

--- routes/admin/user-list/userList.js
+++ routes/admin/user-list/userList.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const userList = require('../../../controllers/admin/user-list/userList')
-const singleUserByName = require('../../../controllers/admin/user-list/singleUserByname')
 
 const router = express.Router()
 
 router.get('/list',userList)
-router.post('/single/byname',singleUserByName)
+
 
 module.exports = router
--- routes/get-users-from-server/allUsersList.js
+++ routes/get-users-from-server/allUsersList.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const allUsers = require('../../controllers/get-user-list-server/allUsers')
 const filePath = require('../../middlewares/setimage-path/imagePath')
+const singleUserByName= require('../../controllers/admin/user-list/singleUserByname')
 const router = express.Router()
 
 
 router.get('/users/list',filePath,allUsers)
+router.post('/single/byname',singleUserByName)
 
 
 module.exports = router
