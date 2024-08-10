303b3e9df5193a8c689049325d753010ac7a9920
muhammad ahmad <110842437+Ahmad4433@users.noreply.github.com>
2024-08-10 08:25:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
-// app.use(cors());
+// app.use(cors({origin:'*'}));
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', '*');
 //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
